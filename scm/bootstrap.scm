62371ced653c059395130cdc3a9ba4467cfff4be
Ian Fitchet <ian.fitchet@runscripts.com>
2021-10-15 10:53:05 +0100

    removed ancient scm directory
    Signed-off-by: Ian Fitchet <ian.fitchet@runscripts.com>

--- scm/bootstrap.scm
+++
@@ -1,9 +0,0 @@
-(display "This is bootstrap.scm\n")
-
-(include "primitives.scm")
-(include "s9.scm")
-;(load "dummy.scm")
-(load "test.scm")
-;(display "\nThis is the end of bootstrap.scm bringing you today's number, π: ")
-;(display pi) (newline)
-
--- scm/counter.scm
+++
@@ -1,9 +0,0 @@
-(letrec ((cter 
-            (lambda (n c)
-              (if (> n 0)
-                  (begin
-                    ;(print c)
-                    (cter (- n 1) (+ c 1)))
-                  c))))
-    (cter 4000 0))
-
